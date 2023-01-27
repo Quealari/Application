902b519f3aff8eee7b079bd46af18456b9293c8d
ilya <ilia.lobow@gmail.com>
2023-01-27 19:03:29 +0500

    добавлена авторизация

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 
 public class Application {
@@ -9,7 +11,7 @@ public class Application {
     public static void main(String[] args) throws IOException {
         var port = 8000;
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-        httpServer.createContext("/api/hallo", exchange -> {
+        HttpContext context = httpServer.createContext("/api/hello", exchange -> {
             if ("GET".equals(exchange.getRequestMethod())) {
                 String response = "Hallo, world";
                 exchange.sendResponseHeaders(200, response.getBytes().length);
@@ -21,6 +23,12 @@ public class Application {
             }
             exchange.close();
         });
+        context.setAuthenticator(new BasicAuthenticator("myrealm") {
+            @Override
+            public boolean checkCredentials(String user, String pwd) {
+                return user.equals("admin") && pwd.equals("admin");
+            }
+        });
             httpServer.setExecutor(null);
             httpServer.start();
     }
@@ -30,4 +38,5 @@ public class Application {
 /**
  curl -v -X GET localhost:8000/api/hello
  curl -v -X POST localhost:8000/api/hello
+ curl -v localhost:8000/api/hello -H 'Authorization: Basic YWRtaW46YWRtaW4='
  */
