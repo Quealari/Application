25776d599f3e86122f3c1fa8e5ffa85c68dceb01
ilya <ilia.lobow@gmail.com>
2023-01-27 18:19:05 +0500

    добавлена поддержка методов

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,16 +9,25 @@ public class Application {
     public static void main(String[] args) throws IOException {
         var port = 8000;
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-        httpServer.createContext("/api/test", exchange -> {
-            String response = "Hallo";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream output = exchange.getResponseBody();
-            output.write(response.getBytes());
-            output.flush();
-            exchange.close();});
-        httpServer.setExecutor(null);
-        httpServer.start();
-
+        httpServer.createContext("/api/hallo", exchange -> {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String response = "Hallo, world";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(response.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        });
+            httpServer.setExecutor(null);
+            httpServer.start();
     }
 
 }
+
+/**
+ curl -v -X GET localhost:8000/api/hello
+ curl -v -X POST localhost:8000/api/hello
+ */
